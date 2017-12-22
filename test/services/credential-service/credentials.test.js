c5ad559176ef8b26af3d0f2d9092a03218122f27
Vincenzo Chianese <vincenz.chianese@icloud.com>
2017-12-22 11:54:03 +0100

    Re-register schemas for credential tests

--- test/services/credential-service/credentials.test.js
+++ test/services/credential-service/credentials.test.js
@@ -1,6 +1,7 @@
 const should = require('should');
 const config = require('../../../lib/config');
 const services = require('../../../lib/services');
+const schemas = require('../../../lib/schemas');
 const credentialService = services.credential;
 const userService = services.user;
 const db = require('../../../lib/db');
@@ -153,7 +154,7 @@ describe('Credential service tests', () => {
   });
 
   describe('Credential Property tests', () => {
-    const originalModelConfig = config.models.credentials;
+    const originalModelConfig = JSON.parse(JSON.stringify(config.models.credentials));
     const username = 'someUser';
     const _credential = {
       secret: 'password',
@@ -162,23 +163,20 @@ describe('Credential service tests', () => {
     };
 
     before(() => {
-      config.models.credentials.oauth2 = {
-        passwordKey: 'secret',
-        autoGeneratePassword: true,
-        type: 'object',
-        properties: {
-          scopes: { type: 'string' },
-          someProperty: { type: 'string' },
-          otherProperty: { type: 'string', default: 'someDefaultValue' }
-        },
-        required: ['someProperty']
-      };
+      Object.assign(config.models.credentials.properties.oauth2.properties, {
+        scopes: { type: 'string' },
+        someProperty: { type: 'string' },
+        otherProperty: { type: 'string', default: 'someDefaultValue' }
+      });
 
+      config.models.credentials.properties.oauth2.required.push('someProperty');
+      schemas.register('credentials', 'credential', config.models.credentials);
       return db.flushdb();
     });
 
     after(() => {
       config.models.credentials = originalModelConfig;
+      schemas.register('credentials', 'credential', config.models.credentials);
     });
 
     it('should not insert a credential with scopes if the scopes are not defined', () => {
@@ -253,8 +251,6 @@ describe('Credential service tests', () => {
     });
 
     it('should not create credential if a required property is not passed in', () => {
-      config.models.credentials.properties.oauth2.required.push('someProperty');
-
       const username3 = 'anotherUser';
       const cred = {
         secret: 'password',
@@ -263,7 +259,7 @@ describe('Credential service tests', () => {
 
       return should(credentialService
         .insertCredential(username3, 'oauth2', cred))
-        .be.rejectedWith('data should have required property \'.someProperty\'')
+        .be.rejectedWith('data should have required property \'someProperty\'')
         .then(() => credentialService.getCredential(username3, 'oauth2'))
         .then(credential => should.not.exist(credential));
     });
